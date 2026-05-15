dc49245bbdc62cffcf5fce1ae09d32babcaa5d93
Raman <ramanthakur8404@gmail.com>
2026-05-15 15:54:52 +0530

    [Raman] : Added MediaServiceApplicationTests.java

---
+++ media-service/src/test/java/com/connectsphere/media_service/MediaServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.connectsphere.media_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class MediaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
