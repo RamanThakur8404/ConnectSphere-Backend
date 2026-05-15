76102d819ae4e5efd53c6b3a361433e42bd74254
Raman <ramanthakur8404@gmail.com>
2026-05-15 15:09:26 +0530

    [Raman] : Added NotificationDeepLinkConstants.java

---
+++ notification-service/src/main/java/com/connectsphere/notification_service/constants/NotificationDeepLinkConstants.java
@@ -0,0 +1,7 @@
+package com.connectsphere.notification_service.constants;
+
+public class NotificationDeepLinkConstants {
+	public static final String DEEPLINK_POST = "/posts/";
+	public static final String DEEPLINK_COMMENT = "/posts/%d#comment-%d";
+	public static final String DEEPLINK_PROFILE = "/users/";
+}
