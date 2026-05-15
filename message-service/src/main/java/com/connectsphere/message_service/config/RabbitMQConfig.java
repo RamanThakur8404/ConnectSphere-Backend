57132bde461bf00c62ccfef64416f807be943ad7
Raman <ramanthakur8404@gmail.com>
2026-05-15 15:13:38 +0530

    [Raman] : Added RabbitMQConfig.java

---
+++ message-service/src/main/java/com/connectsphere/message_service/config/RabbitMQConfig.java
@@ -0,0 +1,27 @@
+package com.connectsphere.message_service.config;
+
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String NOTIF_EXCHANGE = "notification.events";
+    public static final String ROUTING_NOTIF_INAPP = "notif.inapp";
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(jsonMessageConverter());
+        return template;
+    }
+}
