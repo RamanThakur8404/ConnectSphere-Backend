55bb4fae55953a30d233e1bb6f267fc5e5bb7833
Raman <ramanthakur8404@gmail.com>
2026-05-15 15:43:37 +0530

    [Raman] : Added DuplicateReportException.java

---
+++ report-service/src/main/java/com/connectsphere/report_service/exception/DuplicateReportException.java
@@ -0,0 +1,5 @@
+package com.connectsphere.report_service.exception;
+
+public class DuplicateReportException extends RuntimeException {
+    public DuplicateReportException(String message) { super(message); }
+}
